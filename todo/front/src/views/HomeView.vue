b827a2b4783c58697b86e941ca4fde7d5358e9c2
Jong Hak Kim <whdgkr9070@naver.com>
2024-11-15 17:49:53 +0900

    Add ToDo List View

--- todo/front/src/views/HomeView.vue
+++ todo/front/src/views/HomeView.vue
@@ -1,7 +1,36 @@
 <script setup lang="ts">
+import axios from 'axios'
+import { ref } from 'vue'
+
+const todos = ref([])
+
+axios.get('/api/todos?page=1&size=5').then(response => {
+  response.data.forEach((r: any) => {
+    todos.value.push(r);
+  })
+})
 </script>
 
 <template>
-  <main>
-  </main>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      <div>
+        {{ todo.title }}
+      </div>
+
+      <div>
+        {{ todo.content }}
+      </div>
+    </li>
+  </ul>
 </template>
+
+<style scoped>
+li {
+  margin-bottom: 1rem;
+}
+
+li:last-child {
+  margin-bottom: 0;
+}
+</style>
--- todo/front/src/views/WriteView.vue
+++ todo/front/src/views/WriteView.vue
@@ -1,27 +1,38 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import { ref } from 'vue'
 
 import axios from 'axios'
+import { useRouter } from 'vue-router'
 
-const title = ref("")
-const content = ref("")
+const title = ref('')
+const content = ref('')
+
+const router = useRouter()
 
 const write = function() {
-  axios.post("/api/todos", {
+  axios.post('/api/todos', {
     title: title.value,
     content: content.value
   })
+    .then(() => {
+      router.replace
+      (
+        {
+          name: 'home'
+        }
+      )
+    })
 }
 
 </script>
 
 <template>
   <div>
-    <el-input v-model="title" placeholder="제목을 입력해주세요"/>
+    <el-input v-model="title" placeholder="제목을 입력해주세요" />
   </div>
 
   <div class="mt-2">
-    <el-input v-model="content" type="textarea" rows="15"/>
+    <el-input v-model="content" type="textarea" rows="15" />
   </div>
 
   <div class="mt-2">
