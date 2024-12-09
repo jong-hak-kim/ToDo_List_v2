cf7e945570431223a66355fef283c053a4ed0997
Jong Hak Kim <whdgkr9070@naver.com>
2024-12-09 19:01:01 +0900

    feat: Implement functionality to retrieve authenticated user information after login

--- todo/src/main/java/com/todo/controller/MainController.java
+++ todo/src/main/java/com/todo/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.todo.controller;
 
+import com.todo.config.UserPrincipal;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +14,7 @@ public class MainController {
     }
 
     @GetMapping("/user")
-    public String user() {
+    public String user(@AuthenticationPrincipal UserPrincipal userPrincipal) {
         return "사용자 페이지입니다. 🥹";
     }
 
