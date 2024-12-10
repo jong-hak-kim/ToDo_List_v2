33f6dd8b931d4124b561f19089bdfa3104602874
Jong Hak Kim <whdgkr9070@naver.com>
2024-12-10 15:35:06 +0900

    feat: Enable method-level security

--- todo/src/main/java/com/todo/controller/MainController.java
+++ todo/src/main/java/com/todo/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.todo.controller;
 
 import com.todo.config.UserPrincipal;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +14,16 @@ public class MainController {
         return "메인 페이지입니다.";
     }
 
+    //Sprign EL 지원
+    //SecurityConfig에서 @EnableMethodSecurity를 선언하게 되면
+    //PrePostEnabled가 true로 돼서 접속할 수 있는 역할 설정 가능
+    @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/user")
     public String user(@AuthenticationPrincipal UserPrincipal userPrincipal) {
         return "사용자 페이지입니다. 🥹";
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/admin")
     public String admin() {
         return "관리자 페이지입니다. 😍";
